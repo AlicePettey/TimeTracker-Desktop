687b1443c8a9bde17af0ccf82ed794612b7eaa3b
Alice Pettey <alice@alicepettey.com>
2025-12-30 10:36:49 -0500

    1.8.8.2

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  build: {
-    outDir: "vite-dist",
-    emptyOutDir: true,
-  }
   server: {
     host: "::",
     port: 8080,
